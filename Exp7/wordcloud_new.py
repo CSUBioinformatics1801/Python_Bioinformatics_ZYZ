b1c5f55902b32e6045bb5f778d6df85ad5f4330f
Sean Peldom Zhang <51025363+Peldom@users.noreply.github.com>
2021-06-19 23:32:29 +0800

    Update wordcloud_new.py

--- Exp7/wordcloud_new.py
+++ Exp7/wordcloud_new.py
@@ -17,7 +17,7 @@ wlist.sort(key=lambda x:wtimes[x],reverse=True)
 for a in wlist[:10]:
     print(a,wtimes[a],sep='\t')
 text=' '.join(cstr)
-cloud=wordcloud.WordCloud(font_path='D:\\Remain\\7.python及其生物信息学应用\\实践\\实践7\\simsun.ttc',background_color='white',
+cloud=wordcloud.WordCloud(font_path='simsun.ttc',background_color='white',
                           stopwords=sw,collocations=False,width=640,height=480).generate(text)
 file=cloud.to_file('herocloud.png')
 root=tkinter.Tk()
